af3b5cbb7aef5506bad1f56c47ab00fa82c687ac
sugudon <sugufloyd01@gmail.com>
2025-03-01 14:15:28 +0530

    Login, ForgotPassword  Page creation using material UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
-import Button from '@mui/material/Button';
+import { Routes, Route } from 'react-router-dom';
+import Login from './components/Subscription/Login'
 
 export default function App() {
-  return <Button variant="contained">Hello world</Button>;
+  return <>
+    <Routes>
+      <Route path="/" element={<Login />} />
+    </Routes>
+  </>;
 }
