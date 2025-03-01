ef1ae32a2a371b1a139f2c94ea7d253b070c3fcf
sugudon <sugufloyd01@gmail.com>
2025-03-01 18:26:04 +0530

    Read Me added

--- README.md
+++ README.md
@@ -10,8 +10,28 @@
 
 # 3. Static Pages implemented => Login, Register, Forgot Password, Dashboard
 
-# 4. Form Validation Done for the input fields, Phone number 
+# 4. Form Validation Done for the input fields, Phone number
 
 # 5. React Redux Toolkit setup done
 
 # 6. Store configured
+
+# 7. Integrated Rupa sree's Work - landing page
+
+# Do following to see the output
+
+# 1. npm i
+
+# 2. npm run dev
+
+# 3. You can see the landing page and at the top right corner login icon
+
+# My Routes
+
+# 1. /login & forgot password
+
+# 2. /register
+
+# 3. /dashboard and the other rootes under dashboard
+
+# We were unable to complete the entire task as we were discussing the doubts with the team and faced challenges in configuring Git, setting up Node, and installing the other dependencies in the system. Anyway, thank you for this opportunity
--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,17 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './components/Subscription/Login'
 import Register from './components/Subscription/Register'
 import Dashboard from './components/Dashboard/Dashboard'
+import Home from './components/LandingScreen/landingScreen'
+import HomePage from './components/LandingScreen/home'
 
 export default function App() {
   return <>
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/Home" element={<HomePage />} />
+      <Route path="/Health Topics" element={<HomePage />} />
+      <Route path="/Services" element={<HomePage />} />
+      <Route path="/Contact" element={<HomePage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/dashboard" element={<Dashboard />} />
